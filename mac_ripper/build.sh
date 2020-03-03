d793619550e3f85f3b1e1f20cb1984c623b3341a
stokuda <tokuda.sss@gmail.com>
2020-03-04 07:16:39 +0900

    aa

--- mac_ripper/build.sh
+++ mac_ripper/build.sh
@@ -3,7 +3,7 @@ DIST='./automator/MacRipper.app/Contents/MacOS';
 STUB='Application Stub';
 SRC="./src";
 PIP_PACHAGES="./pip-packages";
-BIN_PYTHON="./python";
+#BIN_PYTHON="./python";
 #BREW_PACKAGES="./brew-packages";
 
 cp -p "${DIST}/${STUB}" . \
@@ -11,16 +11,16 @@ cp -p "${DIST}/${STUB}" . \
 && mkdir -p $DIST \
 && cp -r $SRC/* $DIST \
 && cp -r $PIP_PACHAGES $DIST \
-&& cp -r $BIN_PYTHON $DIST \
 && mv "$STUB" $DIST;
 #&& cp -r $BREW_PACKAGES $DIST \
+#&& cp -r $BIN_PYTHON $DIST \
 
-pkgbuild \
-	--identifier MacRipper \
-	--root ./automator/MacRipper.app \
-	--install-location /Applications/MacRipper.app \
-	--version 1.0.0 \
-	--scripts ./scripts \
-	MacRipper.pkg
+#pkgbuild \
+#	--identifier MacRipper \
+#	--root ./automator/MacRipper.app \
+#	--install-location /Applications/MacRipper.app \
+#	--version 1.0.0 \
+#	--scripts ./scripts \
+#	MacRipper.pkg
 #	--ownership preserve \
 #	--install-location /Applications \
